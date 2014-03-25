9dac4a7fdcf58c0e95e52232526dea4a24cb296c
Alex Seewald <alex@openshadow.com>
2014-03-25 11:58:00 +0000

    Additional amends so it compiles on iOS

--- samples/c/build_all.sh
+++ samples/c/build_all.sh
@@ -3,14 +3,14 @@
 if [ $# -gt 0 ] ; then
     base=`basename $1 .c`
     echo "compiling $base"
-    gcc -ggdb `pkg-config opencv --cflags --libs` $base.c -o $base
+    gcc -L /opt/local/lib -ggdb `pkg-config opencv --cflags --libs` $base.c -o $base
 else
     for i in *.c; do
         echo "compiling $i"
-        gcc -ggdb `pkg-config --cflags opencv` -o `basename $i .c` $i `pkg-config --libs opencv`;
+        gcc -L /opt/local/lib -ggdb `pkg-config --cflags opencv` -o `basename $i .c` $i `pkg-config --libs opencv`;
     done
     for i in *.cpp; do
         echo "compiling $i"
-        g++ -ggdb `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
+        g++ -L /opt/local/lib -ggdb `pkg-config --cflags opencv` -o `basename $i .cpp` $i `pkg-config --libs opencv`;
     done
 fi
